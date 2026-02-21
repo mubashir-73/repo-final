195b6daf4ced8cba2457abcaf3ce02e9ec4b1305
mubashir-73 <mubashirsheriff7@gmail.com>
2026-02-21 18:45:31 +0530

    fix redirect uri

--- server/authRoutes.js
+++ server/authRoutes.js
@@ -6,30 +6,29 @@ const router = express.Router();
 
 router.get(
   "/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
+  passport.authenticate("google", { scope: ["profile", "email"] }),
 );
 
-router.get(
-  "/google/callback",
-  (req, res, next) => {
-    passport.authenticate("google", { session: false }, (err, user, info) => {
-      if (err) {
-        console.error("Google Auth Error:", err);
-        return res.redirect("http://localhost:5173/?error=Authentication failed");
-      }
-      if (!user) {
-        return res.redirect("http://localhost:5173/?error=User not registered");
-      }
-
-      const token = jwt.sign(user, process.env.JWT_SECRET, {
-        expiresIn: "1d",
-      });
-
-      res.redirect(
-        `http://localhost:5173/oauth-success?token=${token}`
+router.get("/google/callback", (req, res, next) => {
+  passport.authenticate("google", { session: false }, (err, user, info) => {
+    if (err) {
+      console.error("Google Auth Error:", err);
+      return res.redirect(
+        "https://report.forese.co.in/?error=Authentication failed",
       );
-    })(req, res, next);
-  }
-);
+    }
+    if (!user) {
+      return res.redirect(
+        "https://report.forese.co.in/?error=User not registered",
+      );
+    }
+
+    const token = jwt.sign(user, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.redirect(`https://report.forese.co.in/oauth-success?token=${token}`);
+  })(req, res, next);
+});
 
 export default router;
